5eecbf49a41162865159cec33974425945a263eb
Mike Menne <mpmenne@electronicsystemssupport.com>
2016-02-03 23:27:06 -0600

    Update serializer-json.R

--- R/serializer-json.R
+++ R/serializer-json.R
@@ -1,11 +1,9 @@
 jsonSerializer <- function(val, req, res, errorHandler){
   tryCatch({
       json <- val
-      class(json) <- "json"
 
     res$setHeader("Content-Type", "application/json")
     res$body <- json
-    class(res$body) <- "json"
 
     return(res$toResponse())
   }, error=function(e){
